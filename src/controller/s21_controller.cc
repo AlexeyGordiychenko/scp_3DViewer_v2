dab1ce0532914f5523045254f6960470ba8996c1
elidacon <elidacon@student.21-school.ru>
2023-11-23 23:08:44 +0500

    Add the controller files

--- src/controller/s21_controller.cc
+++ src/controller/s21_controller.cc
@@ -1,3 +1,47 @@
 #include "s21_controller.h"
 
 #include "../model/s21_model.h"
+
+void s21::Controller::Initialize(const std::string filename) {
+  model_->Initialize(filename);
+}
+
+void s21::Controller::RestoreVertices() { model_->RestoreVertices(); }
+
+void s21::Controller::AffineMove(double ax, double ay, double az) {
+  model_->AffineMove(ax, ay, az);
+}
+
+void s21::Controller::AffineRotateX(double angle) {
+  model_->AffineRotateX(angle);
+}
+
+void s21::Controller::AffineRotateY(double angle) {
+  model_->AffineRotateY(angle);
+}
+
+void s21::Controller::AffineRotateZ(double angle) {
+  model_->AffineRotateZ(angle);
+}
+
+void s21::Controller::AffineScale(double k) { model_->AffineScale(k); }
+
+size_t s21::Controller::GetPolygonsEdgesCount() const {
+  return model_->GetPolygonsEdgesCount();
+}
+
+size_t s21::Controller::GetVerticesCount() const {
+  return model_->GetVerticesCount();
+}
+
+const std::vector<std::vector<int>>& s21::Controller::GetPolygons() const {
+  return model_->GetPolygons();
+}
+
+const std::vector<s21::Vertex3d>& s21::Controller::GetVertices() const {
+  return model_->GetVertices();
+}
+
+double s21::Controller::GetCenterX() const { return model_->GetCenterX(); }
+double s21::Controller::GetCenterY() const { return model_->GetCenterY(); }
+double s21::Controller::GetCenterZ() const { return model_->GetCenterZ(); }
--- src/controller/s21_controller.h
+++ src/controller/s21_controller.h
@@ -1,5 +1,46 @@
 #ifndef S21_3DVIEWER_V2_CONTROLLER_H
 #define S21_3DVIEWER_V2_CONTROLLER_H
 
-namespace s21 {}  // namespace s21
-#endif            // S21_3DVIEWER_V2_CONTROLLER_H
+#include <string>
+#include <vector>
+
+#include "../model/s21_vertex3d.h"
+
+namespace s21 {
+
+class Model;
+
+class Controller {
+ public:
+  // Constructors and the destructor
+  Controller(Model* model) : model_(model) {}
+  Controller(const Controller&) = delete;
+  Controller& operator=(const Controller&) = delete;
+  Controller(Controller&&) = delete;
+  Controller& operator=(Controller&&) = delete;
+  ~Controller() = default;
+
+  // Main functions
+  void Initialize(const std::string filename);
+  void RestoreVertices();
+
+  void AffineMove(double ax, double ay, double az);
+  void AffineRotateX(double angle);
+  void AffineRotateY(double angle);
+  void AffineRotateZ(double angle);
+  void AffineScale(double k);
+
+  size_t GetPolygonsEdgesCount() const;
+  size_t GetVerticesCount() const;
+  const std::vector<std::vector<int>>& GetPolygons() const;
+  const std::vector<s21::Vertex3d>& GetVertices() const;
+
+  double GetCenterX() const;
+  double GetCenterY() const;
+  double GetCenterZ() const;
+
+ private:
+  Model* model_;
+};
+}  // namespace s21
+#endif  // S21_3DVIEWER_V2_CONTROLLER_H
