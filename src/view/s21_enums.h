cf9ad8d3325e79e8cb42b6fcfc1fbdf275e3f623
elidacon <elidacon@student.21-school.ru>
2023-11-30 20:45:14 +0500

    Add s21_enums.h

---
+++ src/view/s21_enums.h
@@ -0,0 +1,11 @@
+#ifndef S21_3DVIEWER_V2_ENUMS_H
+#define S21_3DVIEWER_V2_ENUMS_H
+
+namespace s21 {
+
+enum ProjectionType { kParallel, kCentral };
+enum PolygonType { kSolid, kDashed };
+enum VerticeType { kNone, kCircle, kSquare };
+
+}  // namespace s21
+#endif  // S21_3DVIEWER_V2_ENUMS_H
